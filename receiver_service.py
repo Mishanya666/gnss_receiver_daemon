1213aa305d683a184d90fe36ab5a4f423267a0f9
Mishanya666 <149495519+Mishanya666@users.noreply.github.com>
2024-10-29 12:39:16 +0800

    Update receiver_service.py

--- receiver_service.py
+++ receiver_service.py
@@ -1,97 +1,92 @@
 import os
-import sys
-import datetime
-import paho.mqtt.client as mqtt_client
 import time
-import threading
-import random
+import paho.mqtt.client as mqtt_client
+from gnss_tec import rnx
+import warnings
+import glob
+import sys
+from daemonize import Daemonize
+import logging
 
+logging.basicConfig(filename='/tmp/gnss_receiver.log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 broker = "broker.emqx.io"
 topic_prefix = "gnss/data/"
+data_dir = "/mnt/c/Users/dacko/gnss_receiver_daemon/rnx_files"
 
-processed_messages = set()
-subscription_event = threading.Event()
+class GNSSReceiverDaemon:
+    def __init__(self, receiver_name):
+        self.receiver_name = receiver_name
+        self.current_file_path = None
+        self.client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION2,receiver_name)
+#        self.client.on_connect = self.on_connect
+#        self.client.on_disconnect = self.on_disconnect
+        self.client.connect(broker)
+        self.client.loop_start()
+        self.start_processing()
+        logging.info("GNSS Receiver Daemon инициализирован.")
 
-def on_message(client, userdata, message):
-    data = str(message.payload.decode("utf-8"))
-    if data in processed_messages:
-        return
-    try:
-        parts = data.split(' ', 2)
-        if len(parts) < 3:
-            raise ValueError("Invalid message format")
+#    def on_connect(self, client, userdata, flags, rc):
+#        logging.info(f"Подключено к MQTT брокеру с кодом результата {rc}")
+#        print(f"Connected to MQTT broker with result code {rc}")
+#        self.start_processing()
 
-        original_date_str, original_time_str, rest_data = parts
-        original_time_str = f"{original_date_str} {original_time_str}"
+#    def on_disconnect(self, client, userdata, rc):
+#        logging.warning(f"Отключено от MQTT брокера с кодом результата {rc}")
+#        print(f"Disconnected from MQTT broker with result code {rc}")
 
-        original_time = datetime.datetime.strptime(original_time_str, "%Y-%m-%d %H:%M:%S")
-        current_time = datetime.datetime.utcnow()
+    def start_processing(self):
+        logging.info("Начинаю обработку файлов...")
+        while True:
+            if not self.current_file_path or not os.path.exists(self.current_file_path):
+                self.find_and_process_new_file()
+            time.sleep(60)
+    
+    def find_and_process_new_file(self):
+        logging.info("Поиск новых файлов...")
+        pattern = os.path.join(data_dir, self.receiver_name, f"{self.receiver_name}_R_*.rnx")
+        matching_files = glob.glob(pattern)
+        if matching_files:
+            logging.info(f"Найдены файлы: {matching_files}")
+            new_file_path = matching_files[0]
+            if new_file_path != self.current_file_path:
+                self.current_file_path = new_file_path
+                logging.info(f"Обработка файла: {self.current_file_path}")
+                self.process_file(self.current_file_path)
+        else:
+            logging.info(f"No matching files found for pattern: {pattern}")
 
-        if abs((current_time - original_time).total_seconds()) <= 30:
-            processed_messages.add(data)
-            print(f"Processed message: {data}")
-    except ValueError as e:
-        print(f"Error parsing message: {e} - {data}")
-    except Exception as e:
-        print(f"Unexpected error: {e} - {data}")
-
-client = mqtt_client.Client('user')
-client.on_message = on_message
-
-def subscribe_to_topic(receiver_name):
-    try:
-        client.connect(broker)
-        client.loop_start()
-        client.subscribe(f"{topic_prefix}{receiver_name}")
-        print(f"Subscribed to topic: {topic_prefix}{receiver_name}")
-        subscription_event.set()
-    except Exception as ex:
-        print(f"Error subscribing to topic: {ex}")
-        client.disconnect()
-
-def publish_simulated_data(receiver_name):
-    subscription_event.wait()
-    current_time = datetime.datetime.utcnow()
-    start_time = current_time.replace(second=0, microsecond=0)
-
-    while True:
-        message_time = start_time + datetime.timedelta(seconds=30)
-        if datetime.datetime.utcnow() >= message_time:
-            gnss_list = ["G01", "G02", "G03", "G04", "G05", "G06", "G07", "G08", "G09", "G10",
-                         "G11", "G12", "G13", "G14", "G15", "G16", "G17", "G18", "G19", "G20"]
-            selected_gnss = random.choice(gnss_list)
-            phase_tec = random.uniform(-500.0, 500.0)
-            p_range_tec = random.uniform(-50.0, 50.0)
-
-            message = f"{message_time.strftime('%Y-%m-%d %H:%M:%S')} {selected_gnss}: {phase_tec} {p_range_tec}"
-            if message not in processed_messages:
-                try:
-                    client.publish(f"{topic_prefix}{receiver_name}", message)
+    def process_file(self, file_path):
+        logging.info(f"Обработка файла: {file_path}")
+        try:
+            with open(file_path) as obs_file:
+                reader = rnx(obs_file)
+                for tec in reader:
+                    message = '{} {}: {} {}'.format(
+                        tec.timestamp, tec.satellite, tec.phase_tec, tec.p_range_tec
+                    )
+                    self.client.publish(f"{topic_prefix}{self.receiver_name}", message)
+                    logging.info(f"Опубликовано сообщение: {message}")
                     print(f"Published message: {message}")
-                    processed_messages.add(message)
-                except Exception as ex:
-                    print(f"Failed to send message to topic {topic_prefix}{receiver_name}: {ex}")
-            start_time = message_time
-        time.sleep(1)
+                    time.sleep(30)
+        except FileNotFoundError as e:
+            logging.error(f"Ошибка открытия файла: {e}")
+            print(f"Error opening file: {e}")
+        except Exception as e:
+            logging.error(f"Неожиданная ошибка при обработке файла: {e}")
+            print(f"Unexpected error processing file: {e}")
 
-def thread_exception_handler(args):
-    if issubclass(args.exc_type, Exception):
-        print(f"Exception in thread {args.thread.name}: {args.exc_type.__name__} {args.exc_value}")
+    def run(self):
+        logging.info("Запуск демона...")
+        daemon = Daemonize(app=self.receiver_name, pid=f"/tmp/{self.receiver_name}.pid", action=self.start_processing, keep_fds=[])
+        daemon.start()
 
 if __name__ == "__main__":
-    try:
-        receiver_name = input("Enter receiver name to subscribe (or 'exit' to quit): ")
-        if receiver_name.lower() != 'exit':
-            threading.excepthook = thread_exception_handler
-            subscribe_to_topic(receiver_name)
-            publisher_thread = threading.Thread(target=publish_simulated_data, args=(receiver_name,))
-            publisher_thread.start()
-            client.loop_forever()
-        else:
-            sys.exit(0)
-    except KeyboardInterrupt:
-        print("Interrupted by user")
-        client.disconnect()
-    except Exception as ex:
-        print(f"An unexpected error occurred: {ex}")
-        client.disconnect()
+    if len(sys.argv) != 2:
+        print("Usage: python receiver_service.py <receiver_name>")
+        sys.exit(1)
+
+    receiver_name = sys.argv[1]
+    daemon = GNSSReceiverDaemon(receiver_name)
+    daemon.run()
